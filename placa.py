145bf027f1ba8867de14810525b497e646eb3fa7
jfdoppler <jdoppler@df.uba.ar>
2018-10-03 17:33:23 -0300

    Adquisicion con placa NI USB 6210
    Adquisicion de duracion definida seteando el sample rate de la placa
    Conectamos la salida de un generador de funciones a las terminales A/1+ (positiva del generador)
    y A/1- (tierra del generador) (pines 17 y 18 respectivamente)
    
    Testeado (señal y fft) con señales senoidales y cuadradas de entre 1 y 50Hz y 2Vpp

---
+++ placa.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  3 16:14:13 2018
+
+@author: Publico
+"""
+import nidaqmx
+import matplotlib.pyplot as plt
+import numpy as np
+
+# %%
+#with nidaqmx.Task() as task:
+tiempo_medicion = 1
+sample_rate = 10000
+samples_per_channel = sample_rate*tiempo_medicion
+time_vec = np.arange(0, tiempo_medicion, 1/sample_rate)
+med = np.nan
+with nidaqmx.Task() as task:
+    task.ai_channels.add_ai_voltage_chan("Dev6/ai1")
+    task.timing.cfg_samp_clk_timing(sample_rate,
+                                    samps_per_chan=samples_per_channel)
+    med = task.read(number_of_samples_per_channel=samples_per_channel)
+    task.wait_until_done()
+fourier = np.abs(np.fft.rfft(med))
+fourier_freqs = np.fft.rfftfreq(len(med))
+fourier_freqs[np.argmax(fourier)]
+plt.plot(time_vec, med)
+plt.xlabel('tiempo (s)')
+plt.ylabel('tension (V)')
+plt.figure()
+plt.plot(fourier_freqs, fourier)
+plt.ylabel('abs(fourier) (ua)')
+plt.xlabel('frecuencia (Hz)')
