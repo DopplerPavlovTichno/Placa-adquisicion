533980ed9437cf88d2e66f036ff78c3f5c04d6a8
jfdoppler <jdoppler@df.uba.ar>
2018-10-10 18:12:05 -0300

    menor

--- aliasing.py
+++ aliasing.py
@@ -11,16 +11,17 @@ import os
 
 # %%
 tiempo_medicion = 1
-sample_rates = np.arange(10, 500, 10)
+sample_rates = np.arange(1000, 5000, 500)
 frecuencia_picos = []
 mediciones = []
 folder = 'aliasing cuadrada input 50Hz'
+
 for sample_rate in sample_rates:
     samples_per_channel = int(sample_rate*tiempo_medicion)
     time_vec = np.arange(0, tiempo_medicion, 1/sample_rate)
     med = np.nan
     with nidaqmx.Task() as task:
-        task.ai_channels.add_ai_voltage_chan("Dev6/ai1")
+        task.ai_channels.add_ai_voltage_chan("Dev7/ai1")
         task.timing.cfg_samp_clk_timing(sample_rate,
                                         samps_per_chan=samples_per_channel)
         med = task.read(number_of_samples_per_channel=samples_per_channel)
@@ -44,11 +45,11 @@ for sample_rate in sample_rates:
     
     fname = '{}/freqadq{}Hz.dat'.format(folder, sample_rate)
     coment = 'Medimos aliasing. Entrada = cuadrada 2Vpp 50Hz. Variando frecuencia de sampleo de a 10 Hz entre 10 Hz y 500 Hz.'
-    if not os.path.isfile(fname):
-        np.savetxt(fname, np.transpose([time_vec, med]), delimiter = ',', header = 'tiempo (s), tension (V)', footer=coment)
-    else:
-        print('NO GUARDO NADA')
-        print('Ya existe guachin')
+    #if not os.path.isfile(fname):
+    #    np.savetxt(fname, np.transpose([time_vec, med]), delimiter = ',', header = 'tiempo (s), tension (V)', footer=coment)
+    #else:
+    #    print('NO GUARDO NADA')
+    #    print('Ya existe guachin')
 # %%
 #plt.figure()
 #plt.plot(sample_rates, frecuencia_picos, '.-')
