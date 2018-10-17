38925428a6d92287010a85d68c751e752a549d2c
jfdoppler <jdoppler@df.uba.ar>
2018-10-17 18:26:22 -0300

    Corregido calculo de delay

--- multiplexado.py
+++ multiplexado.py
@@ -28,7 +28,7 @@ device = 'Dev7'
 #max_vals = list(-np.array(min_vals))
 for canales_a_borrar in range(0,7):
     canales = ['ai1','ai0','ai7','ai2','ai3','ai4','ai5','ai6']
-    min_vals = [-5.0, -0.5, -5.0, -0.5, -5.0, -0.5, -5.0, -5.0]
+    min_vals = [-5.0, -0.5, -5.0, -0.5, -5.0, -0.5, -5.0, -5.0]  
     max_vals = list(-np.array(min_vals))
     if canales_a_borrar < len(canales) - 1:
         for i in range(canales_a_borrar):
@@ -40,21 +40,22 @@ for canales_a_borrar in range(0,7):
     sample_rate = int(250000/len(canales))
     
     time_vec, med = adquisicion_con_placa.medir(device, canales, sample_rate,
-                                                tiempo_medicion, min_vals = min_vals, max_vals = max_vals)
+                                                tiempo_medicion)#, min_vals = min_vals, max_vals = max_vals)
+    
     corr = signal.correlate(med[0], med[-1], mode='full')
-    delay = np.argmax(corr)/sample_rate
+    delay = (np.argmax(corr)/sample_rate)%max(time_vec)
     fig, ax = plt.subplots(1, figsize=(6, 4))
     ax.plot(time_vec, med[0], label = 'Canal 1')
     ax.plot(time_vec, med[-1], label = 'Canal 6')
     ax.legend()
-#    ax.set_title('{} canales, Delay: {}s'.format(len(canales), delay))
+    ax.set_title('{} canales, Delay: {}s'.format(len(canales), delay))
     fig.tight_layout()
-    
-    fname = '{}/freqgen{}Hz_srate{}_{}canales_consaltosdeganancia.dat'.format(folder, frecuencia, sample_rate, len(canales))
-    coment = 'Medimos una señal de {}Hz en {} canales a fs {} con saltos de ganancia'.format(frecuencia, len(canales), sample_rate)
-    if not os.path.isfile(fname):
-        np.savetxt(fname, np.transpose([time_vec, med[0], med[-1]]), delimiter = ',', header = 'tiempo (s), tension (V)', footer=coment)
-        fig.savefig('{}/{}Hz_{}canales.png'.format(folder, frecuencia, len(canales)))
-    else:
-        print('NO GUARDO NADA')
-        print('Ya existe guachin')
+#    
+#    fname = '{}/freqgen{}Hz_srate{}_{}canales_consaltosdeganancia.dat'.format(folder, frecuencia, sample_rate, len(canales))
+#    coment = 'Medimos una señal de {}Hz en {} canales a fs {} con saltos de ganancia'.format(frecuencia, len(canales), sample_rate)
+#    if not os.path.isfile(fname):
+#        np.savetxt(fname, np.transpose([time_vec, med[0], med[-1]]), delimiter = ',', header = 'tiempo (s), tension (V)', footer=coment)
+#        fig.savefig('{}/{}Hz_{}canales.png'.format(folder, frecuencia, len(canales)))
+#    else:
+#        print('NO GUARDO NADA')
+#        print('Ya existe guachin')
